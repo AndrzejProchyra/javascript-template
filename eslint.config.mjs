f67e59c5b694093ef64e0b89bc0c51165ea8ed39
Andrzej Prochyra <andrzej@prochyra.io>
2024-11-03 15:25:58 +0000

    chore: upgrade ESLint config to use flat config file

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
+import pluginJestExtended from "eslint-plugin-jest-extended";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.spec.js", "**/*.test.js"],
+    plugins: {
+      jest: pluginJest,
+      jestExtended: pluginJestExtended,
+    },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+  },
+  pluginJest.configs["flat/recommended"],
+  pluginJest.configs["flat/style"],
+  pluginJestExtended.configs["flat/all"],
+  eslintConfigPrettier,
+];
