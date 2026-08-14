8f9f79a376be03574da471403586e82853cadda9
Andrzej Prochyra <andrzej@prochyra.io>
2026-08-14 12:52:28 +0100

    build: introduce eslint-plugin-package-json with stylistic rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import pluginJest from "eslint-plugin-jest";
+import packageJson from "eslint-plugin-package-json";
 // import pluginJestExtended from "eslint-plugin-jest-extended";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-/** @type {import("eslint").Linter.Config[]} */
-export default [
+export default defineConfig([
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
+    extends: [
+      pluginJest.configs["flat/recommended"],
+      pluginJest.configs["flat/style"],
+      // pluginJestExtended.configs["flat/all"],
+    ],
     files: ["**/*.spec.js", "**/*.test.js"],
     plugins: {
       jest: pluginJest,
@@ -21,8 +27,9 @@ export default [
       "jest/padding-around-all": "error",
     },
   },
-  pluginJest.configs["flat/recommended"],
-  pluginJest.configs["flat/style"],
-  // pluginJestExtended.configs["flat/all"],
+  {
+    extends: [packageJson.configs.stylistic],
+    files: ["package.json"],
+  },
   eslintConfigPrettier,
-];
+]);
